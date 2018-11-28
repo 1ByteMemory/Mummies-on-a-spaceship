dfba713d06d917be95d8e6fcf7b99cf88349a3f4
1ByteMemory <dan.jones8902@gmail.com>
2018-11-28 11:45:41 +0000

    Added Tutorial

---
+++ Assets/_MyAssets/Scripts/TutorialScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour {
+
+    public GameObject parent;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            parent.SetActive(false);
+        }
+    }
+
+    void Start () {
+        parent.SetActive(true);
+	}
+}
